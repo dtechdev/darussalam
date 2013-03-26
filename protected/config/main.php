953df18a2890571ff001b497568993ef131cc627
zahid <zahidiubb@yahoo.com>
2013-03-26 11:56:05 +0500

    Adding theme functionality against city base

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,7 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Darussalam Publishers',
-       //'theme'=>'memories',
+       'theme'=>'default',
 
           
 	// preloading 'log' component
