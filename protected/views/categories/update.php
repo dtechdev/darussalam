235098c4effe27c415fafacf49dbe5280552285d
zahid <zahidiubb@yahoo.com>
2013-03-28 15:02:04 +0500

    fix parent category listing and add city

--- protected/views/categories/update.php
+++ protected/views/categories/update.php
@@ -18,4 +18,6 @@ $this->menu=array(
 
 <h1>Update Categories <?php echo $model->category_id; ?></h1>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php echo $this->renderPartial('_form', array('model'=>$model,
+                        'categoriesList'=>$categoriesList,
+                        'cityList'=>$cityList)); ?>
