f92d991f8ea8bfe52b30ee9ef2ec9668977d6dc5
ubaid <ubaidullah@darussalampk.com>
2013-04-04 09:34:25 +0500

    main for google plus

--- protected/config/main _local.php
+++ protected/config/main _local.php
@@ -44,7 +44,7 @@ return array(
                     ),
                     "Google" => array(
                         "enabled" => true,
-                        "keys" => array("id" => "561516674806.apps.googleusercontent.com", "secret" => "6IltULRULS7DQSgBpMi05LOD"),
+                        "keys" => array("id" => "602525919522-btoe9e878to546igevhc2p2o6qqc0b0f.apps.googleusercontent.com", "secret" => "r5R8LrY5wMsYXi7MZ6JiH6u8"),
                         "scope" => ""
                     ),
                     "Facebook" => array(
