49faacb10dd6e5b48561a62fb7165a8708ded69a
ubaid <ubaidullah@darussalampk.com>
2013-04-08 10:59:12 +0500

    doctyp

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/style.css" media="screen, projection" />
