f7310c65bd788b724c2d47b315f93e0927a1b111
zahid <zahidiubb@yahoo.com>
2013-04-16 14:57:09 +0500

    register actions

--- protected/controllers/ProductController.php
+++ protected/controllers/ProductController.php
@@ -26,7 +26,7 @@ class ProductController extends Controller {
     public function accessRules() {
         return array(
             array('allow', // allow all users to perform 'index' and 'view' actions
-                'actions' => array('index', 'view', 'addtocart','viewcart','editcart'),
+                'actions' => array('index', 'view', 'addtocart','viewcart','editcart','allproducts','featuredproducts','bestsellings','productdetail','productlisting'),
                 'users' => array('*'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
