b8b24542e4367ab60cf8c61bb2835ce7525016b7
ubaid <ubaidullah@darussalampk.com>
2013-04-22 18:57:58 +0500

    migration

---
+++ protected/migrations/m130422_133105_product_rev_rating.php
@@ -0,0 +1,16 @@
+<?php
+
+class m130422_133105_product_rev_rating extends CDbMigration {
+
+    public function up() {
+        $table = "product_reviews";
+        return true;
+        $this->addColumn($table, "rating", "int(11) DEFAULT NULL");
+    }
+
+    public function down() {
+        $table = "product_reviews";
+        $this->dropColumn($table, "rating");
+    }
+
+}
