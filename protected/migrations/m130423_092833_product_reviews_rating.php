f6c7da189e8eb4e04740a2c7872e083541043d8c
ubaid <ubaidullah@darussalampk.com>
2013-04-23 15:47:43 +0500

    migration

---
+++ protected/migrations/m130423_092833_product_reviews_rating.php
@@ -0,0 +1,27 @@
+<?php
+
+class m130423_092833_product_reviews_rating extends CDbMigration
+{
+	public function up()
+	{
+            $table = "product_reviews";
+        $this->addColumn($table, "avatar", "varchar(255) DEFAULT NULL");
+	}
+
+	public function down()
+	{
+		$table = "product_reviews";
+              $this->dropColumn($table, "rating");
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
