6c31480d005cd3e800419363ed9befb31ebeea49
zahid <zahidiubb@yahoo.com>
2013-04-23 16:17:35 +0500

    create path for hybridth folder

--- protected/modules/yiiauth/YiiauthModule.php
+++ protected/modules/yiiauth/YiiauthModule.php
@@ -1,64 +1,66 @@
 <?php
 
 class YiiauthModule extends CWebModule
-	{
-	private $_assetsUrl;
-	public $userClass;
-	public $config;
-	public function init()
-	{
+{
 
-		// this method is called when the module is being created
-		// you may place code here to customize the module or the application
+    private $_assetsUrl;
+    public $userClass;
+    public $config;
 
-		// import the module-level models and components
-		$this->setImport(array(
-			'yiiauth.models.*',
-			'yiiauth.components.*',
-		));
-	}
-	
-	
-	
-		/**
-	* @return string the base URL that contains all published asset files of this module.
-	*/
-	
-	public function getAssetsUrl()
-	{
-	if($this->_assetsUrl===null)
-	$this->_assetsUrl=Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('admin.assets'));
-	return $this->_assetsUrl;
-	}
+    public function init()
+    {
 
-	/**
-	* @param string the base URL that contains all published asset files of this module.
-	*/
-	public function setAssetsUrl($value)
-	{
-	$this->_assetsUrl=$value;
-	}
+        // this method is called when the module is being created
+        // you may place code here to customize the module or the application
+        // import the module-level models and components
+        $this->setImport(array(
+            'yiiauth.models.*',
+            'yiiauth.components.*',
+        ));
+        
+       $this->config['base_url'] = Yii::app()->request->hostInfo.Yii::app()->baseUrl."/hybridauth";
+    
+    }
 
-	public function registerCss($file, $media='all')
-	{
-	$href = $this->getAssetsUrl().'/css/'.$file;
-	return '<link rel="stylesheet" type="text/css" href="'.$href.'" media="'.$media.'" />';
-	}
+    /**
+     * @return string the base URL that contains all published asset files of this module.
+     */
+    public function getAssetsUrl()
+    {
+        if ($this->_assetsUrl === null)
+            $this->_assetsUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('admin.assets'));
+        return $this->_assetsUrl;
+    }
 
-	public function registerImage($file)
-	{
-	return $this->getAssetsUrl().'/images/'.$file;
-	}
-	
-	public function beforeControllerAction($controller, $action)
-	{
-		if(parent::beforeControllerAction($controller, $action))
-		{
-			// this method is called before any module controller action is performed
-			// you may place customized code here
-			return true;
-		}
-		else
-			return false;
-	}
+    /**
+     * @param string the base URL that contains all published asset files of this module.
+     */
+    public function setAssetsUrl($value)
+    {
+        $this->_assetsUrl = $value;
+    }
+
+    public function registerCss($file, $media = 'all')
+    {
+        $href = $this->getAssetsUrl() . '/css/' . $file;
+        return '<link rel="stylesheet" type="text/css" href="' . $href . '" media="' . $media . '" />';
+    }
+
+    public function registerImage($file)
+    {
+        return $this->getAssetsUrl() . '/images/' . $file;
+    }
+
+    public function beforeControllerAction($controller, $action)
+    {
+        if (parent::beforeControllerAction($controller, $action))
+        {
+            // this method is called before any module controller action is performed
+            // you may place customized code here
+            return true;
+        }
+        else
+            return false;
+    }
+
 }
