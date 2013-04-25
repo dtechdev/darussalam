d7a6c5208d5dc2d9ce95bb020feb50e92b437af3
zahid <zahidiubb@yahoo.com>
2013-04-25 15:05:39 +0500

    credit card payment using authorize.net api

--- protected/modules/web/views/product/payment_method.php
+++ protected/modules/web/views/product/payment_method.php
@@ -1,6 +1,6 @@
 <div id="book_content">
     <div id="book_main_content">
-        <?php $this->renderPartial("_subheader"); ?>
+        <?php $this->renderPartial("_suheader"); ?>
     </div>
 </div>
 <div id="payment_method">
