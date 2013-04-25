4ce44f3df0eb3a0f2fffaf953f1bc3db56dee39e
zahid <zahidiubb@yahoo.com>
2013-04-25 15:09:20 +0500

    credit card payment using authorize.net api

--- protected/modules/web/views/product/payment_method.php
+++ protected/modules/web/views/product/payment_method.php
@@ -1,6 +1,6 @@
 <div id="book_content">
     <div id="book_main_content">
-        <?php $this->renderPartial("_suheader"); ?>
+        <?php $this->renderPartial("_subheader"); ?>
     </div>
 </div>
 <div id="payment_method">
@@ -18,47 +18,106 @@
             </div>
         </div>
         <div class="bottom_payment_method">
+<!--                <form method="POST" action="<?php echo $this->createUrl('/web/Paypal/directpayment'); ?>">-->
+            <?php
+            $form = $this->beginWidget('CActiveForm', array(
+                'id' => 'card-form',
+                'enableClientValidation' => true,
+                'clientOptions' => array(
+                    'validateOnSubmit' => true,
+                ),
+                    ));
+            ?>
+
             <div class="left_method">
                 <h3>Credit Card</h3>
                 <div class="under_left_method">
                     <p>We accept Master Card, Visa, Discover and American Express.</p>
                     <p><span>*</span> First Name</p>
-                    <input type="text" class="payment_text" />
+
+                    <?php echo $form->textField($model, 'first_name', array('class' => 'payment_text','value'=>'zahid')); ?>
+                    <?php echo $form->error($model, 'first_name'); ?>
+
+
                     <p><span>*</span> Last Name</p>
-                    <input type="text" class="payment_text" />
+                    <?php echo $form->textField($model, 'last_name', array('class' => 'payment_text')); ?>
+                    <?php echo $form->error($model, 'last_name'); ?>
+                    <p><span>*</span> Card Type</p>
+                    
+                    <select>
+                            <option>Visa</option>
+                            <option>Master</option>
+                            <option>Discover</option>
+                            <option>American Express</option>
+                        </select>
+
+                    <?php echo $form->textField($model, 'card_type', array('class' => 'small2_text')); ?>
+                    <?php echo $form->error($model, 'card_type'); ?>
+
                     <p><span>*</span>  Card Number <i>(the 16 digits on the front of the card)</i></p>
-                    <input type="text" class="small_text" />
-                    <input type="text" class="small_text" />
-                    <input type="text" class="small_text" />
-                    <input type="text" class="small_text" />
+                    <?php echo $form->textField($model, 'card_number1', array('class' => 'small_text')); ?>
+                    <?php echo $form->error($model, 'card_number1'); ?>
+                    <?php echo $form->textField($model, 'card_number2', array('class' => 'small_text')); ?>
+                    <?php echo $form->error($model, 'card_number2'); ?>
+                    <?php echo $form->textField($model, 'card_number3', array('class' => 'small_text')); ?>
+                    <?php echo $form->error($model, 'card_number3'); ?>
+                    <?php echo $form->textField($model, 'card_number4', array('class' => 'small_text')); ?>
+                    <?php echo $form->error($model, 'card_number4'); ?>
+
+
                     <div class="payment_small_img">
                         <a href="#"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/visa_big_img_03.png" alt="Visa" class="visa_img" /></a>
                         <a href="#"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/master_card_big_img_03.png" alt="Master Mind" class="visa_img" /></a>
                         <a href="#"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/discover_big_img_03.png" alt="Discover" class="visa_img" /></a>
                         <a href="#"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/americanexpress_big_img_03.png" alt="American Express" /></a>
                     </div>
+
                     <p><span>*</span> CVS or CVS <i>(Last 3 digits on back of card, Amex: 4 gigit code on front)</i></p>
-                    <input type="text" class="small2_text" />
+
+                    <?php echo $form->textField($model, 'cvc', array('class' => 'small2_text')); ?>
+                    <?php echo $form->error($model, 'cvc'); ?>
                     <img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/card_img_03.png" alt="scratch card" class="card_img" />
+
                     <p><span>*</span> Expiration Date</p>
                     <div class="payment_option">
                         <p class="monthnyear"> Month </p>
-                        <select>
+                        <select name="exp_month">
                             <option> - </option>
-                            <option> 1 </option>
-                            <option> 2 </option>
+                            <option> 01 </option>
+                            <option> 02 </option>
+                            <option> 03 </option>
+                            <option> 04 </option>
+                            <option> 05 </option>
+                            <option> 06 </option>
+                            <option> 07 </option>
+                            <option> 08 </option>
+                            <option> 09 </option>
+                            <option> 10 </option>
+                            <option> 11 </option>
+                            <option> 12 </option>
                         </select>
                         <span> Year </span>
-                        <select>
+                        <select name="exp_year">
                             <option> - </option>
-                            <option> 1 </option>
-                            <option> 2 </option>
+                            <option> 2013 </option>
+                            <option> 2014 </option>
+                            <option> 2015 </option>
+                            <option> 2016 </option>
+                            <option> 2017 </option>
+                            <option> 2018 </option>
+                            <option> 2019 </option>
+                            <option> 2020 </option>
+                            <option> 2021 </option>
+                            <option> 2022 </option>
+                            <option> 2023 </option>
+                            <option> 2024 </option>
+                            <option> 2025 </option>
                         </select>
                     </div>
                 </div>
                 <div class="paypal">
                     <span>PayPal</span>
-                    <a href="<?php echo $this->createUrl('/web/product/confirmorder', array('type' => 'paypal')); ?>"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/paypal_img_03.png" alt="paypal img" class="paypal_img" /></a>
+                    <a href="<?php echo $this->createUrl('/web/Paypal/buy'); ?>"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/paypal_img_03.png" alt="paypal img" class="paypal_img" /></a>
                 </div>
                 <div class="under_left_method">
                     <input type="checkbox" /><span class="payment_check"> Save your payment method for any future transactions</span>
@@ -74,16 +133,22 @@
                             <td class="right_prefix"><span>*</span> First Name</td>
                         </tr>
                         <tr>
-                            <td class="left_prefix"><select><option>Mr. </option><option>Mrs. </option></select></td>
-                            <td class="right_prefix"><input type="text" class="small_right_text"></td>
+                            <td class="left_prefix"><select name="shipping_prefix"><option>Mr. </option><option>Mrs. </option></select></td>
+                            <td class="right_prefix">
+                                <?php echo $form->textField($model, 'shipping_first_name', array('class' => 'small_right_text')); ?>
+                                <?php echo $form->error($model, 'shipping_first_name'); ?>
+                            </td>
                        	</tr>
                     </table>
                     <p><span>*</span> Last Name</p>
-                    <input type="text" class="payment_text" />
+                    <?php echo $form->textField($model, 'shipping_last_name', array('class' => 'payment_text')); ?>
+                    <?php echo $form->error($model, 'shipping_last_name'); ?>
                     <p><span>*</span> Address Line 1</p>
-                    <input type="text" class="payment_text" />
+                    <?php echo $form->textField($model, 'shipping_address1', array('class' => 'payment_text')); ?>
+                    <?php echo $form->error($model, 'shipping_address1'); ?>
                     <p><span>*</span> Address Line 2</p>
-                    <input type="text" class="payment_text" />
+                    <?php echo $form->textField($model, 'shipping_address2', array('class' => 'payment_text')); ?>
+                    <?php echo $form->error($model, 'shipping_address2'); ?>
                     <p><span>*</span> Country</i></p>
                     <div class="country_option">
                         <select>
@@ -93,7 +158,8 @@
                         </select>
                     </div>
                     <p><span>*</span> City</p>
-                    <input type="text" class="payment_text" placeholder="Enter Your City" />
+                    <?php echo $form->textField($model, 'shipping_city', array('class' => 'payment_text')); ?>
+                    <?php echo $form->error($model, 'shipping_city'); ?>
                     <table width="100%" class="state_table">
                         <tr>
                             <td class="left_state"><span>*</span> State / Province</td>
@@ -101,14 +167,18 @@
                         </tr>
                         <tr>
                             <td class="left_state"><select><option>Select State</option><option>Select State</option><option>Select State</option></select></td>
-                            <td class="right_state"><input type="text" class="zip_text"></td>
+                            <td class="right_state">
+                            <?php echo $form->textField($model, 'shipping_zip', array('class' => 'zip_text')); ?>
+                            <?php echo $form->error($model, 'shipping_zip'); ?>
                         </tr>
                     </table>
                     <p><span>*</span> Telephone Number <i>(10 gigits only, no dashes)</i></p>
-                    <input type="text" class="payment_text" placeholder="Enter Your City" />
+                    <?php echo $form->textField($model, 'shipping_phone', array('class' => 'payment_text')); ?>
+                    <?php echo $form->error($model, 'shipping_phone'); ?>
                 </div>
             </div>
-            <input type="button" value="Continue" name="continue" class="continue" />
-            <a href="<?php echo $this->createUrl('/web/product/confirmorder', array('type' => 'card')); ?>">Credit Card</a>
+            <?php echo CHtml::submitButton('continue',array('class'=>'continue')); ?>
+<!--            <a href="<?php echo $this->createUrl('/web/product/confirmorder', array('type' => 'card')); ?>">Credit Card</a>-->
+<?php $this->endWidget(); ?>
         </div>
     </div>
