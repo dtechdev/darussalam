a38624e6e220e6080fd13982ef15504d314375d6
ubaid <ubaidullah@darussalampk.com>
2013-04-30 13:11:24 +0500

    admin theme added for that views modified

--- protected/views/author/index.php
+++ protected/views/author/index.php
@@ -1,20 +1,54 @@
 <?php
 /* @var $this AuthorController */
-/* @var $dataProvider CActiveDataProvider */
+/* @var $model Author */
 
 $this->breadcrumbs=array(
-	'Authors',
+	'Authors'=>array('index'),
+	'Manage',
 );
 
 $this->menu=array(
+	array('label'=>'List Author', 'url'=>array('index')),
 	array('label'=>'Create Author', 'url'=>array('create')),
-	array('label'=>'Manage Author', 'url'=>array('admin')),
 );
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#author-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
 ?>
 
-<h1>Authors</h1>
+<h1>Add  Authors</h1>
 
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+<p>
+You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+</p>
+
+<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<div class="search-form" style="display:none">
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+)); ?>
+</div><!-- search-form -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'author-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'author_id',
+		'author_name',
+		array(
+			'class'=>'CButtonColumn',
+		),
+	),
 )); ?>
