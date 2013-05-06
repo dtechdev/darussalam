b0627fc270fb999cc589247cd0db44fc904f7209
ahmad <ahmad@ahmad-Inspiron-5521>
2013-05-06 16:12:45 +0500

    js

--- themes/default/js/sign_in.js
+++ themes/default/js/sign_in.js
@@ -1,9 +1,11 @@
 // JavaScript Document
 
-$(document).ready(function(){	
-    $('.example2').hide().before('<a href="#" id="toggle-example2" class="button">Sign In</a>');
-	$('a#toggle-example2').click(function() {
-		$('.example2').slideToggle(0);
-		return false;
-	});
+$(document).ready(function() {
+    if (jQuery('.example2') != null) {
+            jQuery('.example2').hide().before('<a href="#" id="toggle-example2" class="button">Sign In</a>');
+            jQuery('a#toggle-example2').click(function() {
+                jQuery('.example2').slideToggle(0);
+                return false;
+            });
+    }
 });
