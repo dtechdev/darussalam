41eaba22534b3218f21a93a54b6bea56a7042706
ahmad <ahmad@ahmad-Inspiron-5521>
2013-05-08 18:31:01 +0500

    db file

---
+++ protected/config/_config/_conf_db.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Database configurations, A part of component array,
+ * 
+ */
+$conf_component_db = array(
+    'connectionString' => 'mysql:host=localhost;dbname=darussalam', //myprojectnet
+    'emulatePrepare' => true,
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+);
+?>
