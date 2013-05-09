477a1d3614e7e6ce1d8d0d422421ed30a45ccc0c
zahid <zahidiubb@yahoo.com>
2013-05-09 16:01:08 +0500

    Adding translator compiler crud and integrate this on admin side.

--- protected/views/translatorCompiler/_form.php
+++ protected/views/translatorCompiler/_form.php
@@ -27,36 +27,6 @@
 		<?php echo $form->error($model,'type'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'create_time'); ?>
-		<?php echo $form->textField($model,'create_time'); ?>
-		<?php echo $form->error($model,'create_time'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'create_user_id'); ?>
-		<?php echo $form->textField($model,'create_user_id',array('size'=>11,'maxlength'=>11)); ?>
-		<?php echo $form->error($model,'create_user_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'update_time'); ?>
-		<?php echo $form->textField($model,'update_time'); ?>
-		<?php echo $form->error($model,'update_time'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'update_user_id'); ?>
-		<?php echo $form->textField($model,'update_user_id',array('size'=>11,'maxlength'=>11)); ?>
-		<?php echo $form->error($model,'update_user_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'activity_log'); ?>
-		<?php echo $form->textArea($model,'activity_log',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'activity_log'); ?>
-	</div>
-
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
--- protected/views/translatorCompiler/index.php
+++ protected/views/translatorCompiler/index.php
@@ -7,14 +7,15 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
+	array('label'=>'List TranslatorCompiler', 'url'=>array('index')),
 	array('label'=>'Create TranslatorCompiler', 'url'=>array('create')),
-	array('label'=>'Manage TranslatorCompiler', 'url'=>array('admin')),
 );
 ?>
 
 <h1>Translator Compilers</h1>
 
-<?php $this->widget('zii.widgets.CListView', array(
+<?php $this->widget('zii.widgets.grid.CGridView', array(
 	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+        'id' => 'trans-grid',
+	//'itemView'=>'_view',
 )); ?>
