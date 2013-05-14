70fc3a6c426b2e798e0fa452eefbacc6e9356b57
ahmad <ahmad@ahmad-Inspiron-5521>
2013-05-14 17:00:36 +0500

    product detail view customization

--- protected/modules/web/views/product/_product_detail_data.php
+++ protected/modules/web/views/product/_product_detail_data.php
@@ -67,7 +67,7 @@
                       '));
                 } else {
 
-                    echo $product->productLanguage[0]->language_name;
+                    echo $product->productProfile[0]->language_name;
                 }
                 ?>
             </td>
