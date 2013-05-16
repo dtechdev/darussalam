01361ac094641fb94041fb6adb19d798cc6a51d7
ahmad <ahmad@ahmad-Inspiron-5521>
2013-05-16 13:55:18 +0500

    detail page seo

--- protected/modules/web/views/product/all_products.php
+++ protected/modules/web/views/product/all_products.php
@@ -7,4 +7,10 @@ echo $this->renderPartial('product_listing', array(
     'products' => $products,
     'allCate' => $allCate)
 );
+
+?>
+<?php 
+    Yii::app()->clientScript->registerScript('change_cat_script', '
+        dtech.loadallPrdoucts_Cat("'.$this->createUrl("/web/product/productfilter").'");
+    ', CClientScript::POS_READY);
 ?>
