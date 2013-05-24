c17a5372fdf0dce7097eb78c97836e1da00b60e7
ubaid <ubaidullah@darussalampk.com>
2013-05-24 12:16:05 +0500

    deletion of user from usertable

--- protected/migrations/m130524_060123_test_adding_user_usertable.php
+++ protected/migrations/m130524_060123_test_adding_user_usertable.php
@@ -24,7 +24,19 @@ class m130524_060123_test_adding_user_usertable extends CDbMigration {
     }
 
     public function down() {
-        return TRUE;
+        $table = "user";
+        for ($i = 1; $i < 200; $i++) {
+            $columns = array(
+                "user_name" => 'testuser' . $i,
+                "user_password" => md5('admin'),
+                "user_email" => 'test' . $i . '@gmail.com',
+                "role_id" => '3',
+                "status_id" => "1",
+                "city_id" => "1",
+                "site_id" => "1",
+            );
+            $this->delete($table, 'user_name="testuser' . $i . '"');
+        }
     }
 
 }
