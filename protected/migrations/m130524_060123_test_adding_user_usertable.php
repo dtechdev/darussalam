10f6661e77f018350bed149e725135841d942f3f
ubaid <ubaidullah@darussalampk.com>
2013-05-24 12:06:00 +0500

    migrattion for adding 200 users to usertable.....

---
+++ protected/migrations/m130524_060123_test_adding_user_usertable.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Migration for insterting test user in user table 
+ */
+
+class m130524_060123_test_adding_user_usertable extends CDbMigration {
+
+    public function up() {
+
+        $table = "user";
+        for ($i = 1; $i < 200; $i++) {
+            $columns = array(
+                "user_name" => 'testuser' . $i,
+                "user_password" => md5('admin'),
+                "user_email" => 'test' . $i . '@gmail.com',
+                "role_id" => '3',
+                "status_id" => "1",
+                "city_id" => "1",
+                "site_id" => "1",
+            );
+            $this->insert($table, $columns);
+        }
+    }
+
+    public function down() {
+        return TRUE;
+    }
+
+}
