a569c4f822507d8f3108b3614c87277228aab736
ubaid <ubaidullah@darussalampk.com>
2013-05-27 15:48:06 +0500

    sit controller

--- protected/modules/web/views/payment/_paypal.php
+++ protected/modules/web/views/payment/_paypal.php
@@ -1,6 +1,6 @@
 <div class="paypal pay_list">
     <h3>PayPal</h3>
-    <a href="<?php echo $this->createUrl('/web/Paypal/buy'); ?>">
+    <a href="<?php echo $this->createUrl('/web/Paypal/buy'); ?>" style="display:none" >
         <?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/paypal_img_03.png', 'paypal img', array("class" => "paypal_img")) ?>
     </a>
 </div>
