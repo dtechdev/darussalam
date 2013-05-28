05dfba42474f5061cc315fddd77837c5b36a9336
ubaid <ubaidullah@darussalampk.com>
2013-05-28 10:41:47 +0500

    migration for making 100 books featured....

---
+++ protected/migrations/m130528_052729_make_some_products_featured.php
@@ -0,0 +1,24 @@
+<?php
+
+class m130528_052729_make_some_products_featured extends DTDbMigration {
+
+    public function up() {
+
+
+
+        $table = "product";
+        $sql = "SELECT * FROM product 
+                WHERE product_id >=1 AND product_id <=100 ";
+
+        $featured_products = $this->getQueryAll($sql);
+        foreach ($featured_products as $featured) {
+
+            $this->update($table, array('is_featured' => '1'), 'product_id=' . $featured['product_id']);
+        }
+    }
+
+    public function down() {
+        return TRUE;
+    }
+
+}
