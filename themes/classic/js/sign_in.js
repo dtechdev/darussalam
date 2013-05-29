1433c02dee5ac24782ca5816f9248d9d90907986
ubaid <ubaidullah@darussalampk.com>
2013-05-29 09:43:09 +0500

    new design added to themes

--- themes/classic/js/sign_in.js
+++ themes/classic/js/sign_in.js
@@ -1,9 +1,11 @@
 // JavaScript Document
 
-jQuery(document).ready(function(){	
-   jQuery('.example2').hide().before('<a href="#" id="toggle-example2" class="button">Sign In</a>');
-	jQuery('a#toggle-example2').click(function() {
-		jQuery('.example2').slideToggle(0);
-		return false;
-	});
+jQuery(document).ready(function() {
+    if (jQuery('.example2') != null) {
+            jQuery('.example2').hide().before('<a href="#" id="toggle-example2" class="button">Sign In</a>');
+            jQuery('a#toggle-example2').click(function() {
+                jQuery('.example2').slideToggle(0);
+                return false;
+            });
+    }
 });
