8a3c912f2ef021cd428c3003f4880359d34982af
aliabbas <aliabbas@localhost>
2013-05-30 19:01:03 +0500

    db file

--- protected/config/_config/_conf_db.php
+++ protected/config/_config/_conf_db.php
@@ -8,7 +8,7 @@ $conf_component_db = array(
     'connectionString' => 'mysql:host=localhost;dbname=darussalam', //myprojectnet
     'emulatePrepare' => true,
     'username' => 'root',
-    'password' => '',
+    'password' => 'admin',
     'charset' => 'utf8',
 );
 ?>
