8106710051d9384d63c4e619a389d993827db48c
aliabbas <aliabbas@localhost>
2013-06-04 16:13:47 +0500

    web service

--- protected/controllers/WSController.php
+++ protected/controllers/WSController.php
@@ -15,13 +15,13 @@ class WSController extends Controller {
             return true;
         }
         if ($_REQUEST['record_set'] == 'product') {
-            $model = new ProductWS;
-            $allBooks = $model->getWsAllBooks();
+         
+            $allBooks = ProductWS::model()->getWsAllBooks();
             $this->layout = "";
             echo CJSON::encode($allBooks);
         } else if ($_REQUEST['record_set'] == 'product_category') {
-            $model = new ProductWS;
-            $allBooks = $model->getWsAllBooksByCategory();
+            
+            $allBooks = ProductWS::model()->getWsAllBooksByCategory();
 
             $this->layout = "";
             echo CJSON::encode($allBooks);
