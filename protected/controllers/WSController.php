7715399528e9a7c6bf776a51fbbacb30b6bafde2
ubaid <ubaidullah@darussalampk.com>
2013-06-04 14:43:31 +0500

    webservices

--- protected/controllers/WSController.php
+++ protected/controllers/WSController.php
@@ -10,7 +10,7 @@ class WSController extends Controller {
     public $layout = '';
 
     public function actionIndex() {
-       
+
         if ($_REQUEST['record_set'] == 'product') {
             $model = new Product;
             $allBooks = $model->getWsAllBooks();
@@ -19,8 +19,48 @@ class WSController extends Controller {
         } else if ($_REQUEST['record_set'] == 'product_category') {
             $model = new Product;
             $allBooks = $model->getWsAllBooksByCategory();
+
             $this->layout = "";
             echo CJSON::encode($allBooks);
+        }
+    }
+
+    /*
+     * Iphon service to Send All categories
+     * in Daraussalam Database
+     * 
+     */
+
+    public function actionAllCategories() {
+        $categories = Categories::model()->findAll();
+
+        try {
+            $ret_array = array();
+            $ret_array['error'] = '';
+            $ret_array['data'] = $categories;
+            $ret_array['count'] = count($categories);
+            echo CJSON::encode($ret_array);
+        } Catch (Exception $e) {
+            echo CJSON::encode(array("error" => $e->getCode()));
+        }
+    }
+
+    /*
+     * Iphon service to Send All categories
+     * in Daraussalam Database
+     * 
+     */
+
+    public function actionRequestedCategory($category_id = 0) {
+        $requested_cat = ProductWS::model()->getWsRequestByCategory($category_id);
+        try {
+            $requested_product_arr = array();
+            $requested_product_arr['error'] = '';
+            $requested_product_arr['data'] = $requested_cat;
+            $requested_product_arr['count'] = count($requested_cat);
+            echo CJSON::encode($requested_product_arr);
+        } catch (Exception $e) {
+            echo CJSON::encode(array("error" => $e->getCode()));
         }
     }
 
