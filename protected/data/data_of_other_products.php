7ba1890448995dc457ab5ca491e6198b5e37ff8e
ubaid <ubaidullah@darussalampk.com>
2013-06-07 12:16:48 +0500

    new theme

---
+++ protected/data/data_of_other_products.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * php arrray for eductaional toys and others 
+ * products of darussalam
+ * 
+ */
+
+$data = array(
+    0 => array('name' => 'Al-Quran Al-Karim', 'desc' => 'Urdu Software Al-Quran Al-KArim ', 'price' => ' 500.00'),
+    1 => array('name' => 'Al-Quran: Should it be Read wih Understanding?', 'desc' => 'English Video 2-DVDs Set of Dr. Zakir Naik', 'price' => '175.00'),
+    2 => array('name' => 'Hinduism and Islam', 'desc' => 'English Video 2-DVDs Set of Dr. Zakir Naik ', 'price' => '175.00'),
+    3 => array('name' => 'Is The Quran God s Word?', 'desc' => 'Urdu Software Al-Quran Al-KArim ', 'price' => '175.00'),
+    4 => array('name' => 'Press Debate', 'desc' => 'Press Debate ', 'price' => '175.00'),
+    5 => array('name' => 'Tilawat o Terjma Quran (Video)', 'desc' => 'Tilawat o Terjma Quran (Video)', 'price' => '660.00'),
+    6 => array('name' => 'The Noble Quran (4 CDs Set)', 'desc' => 'The Noble Quran (4 CDs Set)', 'price' => ' 190.00'),
+    7 => array('name' => 'Salah: The Programming Towards Righteousness', 'desc' => 'Salah: The Programming Towards Righteousness', 'price' => '175.00'),
+    8 => array('name' => 'Tilawat o Terjma Quran Majeed', 'desc' => 'Tilawat o Terjma Quran Majeed', 'price' => '1850.00'),
+    9 => array('name' => 'Tilawat Quran', 'desc' => 'Tilawat Quran', 'price' => '1200.00'),
+    10 => array('name' => 'Universal Brotherhood', 'desc' => 'Universal Brotherhood', 'price' => '175.00'),
+    11 => array('name' => 'Was Christ Realy Crucified?', 'desc' => 'Was Christ Realy Crucified?', 'price' => '175.00'),
+    12 => array('name' => 'Why the West is Coming to Islam?', 'desc' => 'Why the West is Coming to Islam?', 'price' => '175.00'),
+    13 => array('name' => 'Women s Rights in Islam Modern or Outdated?', 'desc' => 'Women s Rights in Islam Modern or Outdated?', 'price' => '175.00'),
+    14 => array('name' => 'Was Christ Realy Crucified?', 'desc' => 'Was Christ Realy Crucified?', 'price' => '175.00'),
+    15 => array('name' => 'Quran Pen Reader', 'desc' => 'Quran Pen Reader', 'price' => '400.00'),
+);
+?>
