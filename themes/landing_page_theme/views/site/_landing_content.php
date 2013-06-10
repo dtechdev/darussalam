1a2ea5d9a2c7bc11622198f19bd24d01aabca523
ubaid <ubaidullah@darussalampk.com>
2013-06-10 15:12:55 +0500

    new theme

--- themes/landing_page_theme/views/site/_landing_content.php
+++ themes/landing_page_theme/views/site/_landing_content.php
@@ -4,10 +4,10 @@
         <?php //echo CVarDumper::dump($model,10,TRUE);die; ?>
         <div class="landing_div">
             <?php
-            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/quran_img_03.png"), $this->createUrl('/web/product/allproducts'));
+            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/quran_without_color_07.png"), $this->createUrl('/web/quran/index', array('country' => Yii::app()->session['country_short_name'], 'city' => Yii::app()->session['city_short_name'], 'city_id' => Yii::app()->session['city_id'])));
             ?>
             <h4>Quran</h4>
-            <article>Lorem ipsum color sit bla bla thhm ipoum deona eio a ea sho moxnt</article>
+            <article>The Quran is the central religious text of Islam, which Muslims believe to be the verbatim word of God.</article>
             <input type="button" value="Shop Now" class="shop_now" />
         </div>
         <div class="landing_div">
@@ -15,7 +15,7 @@
                 <div class="ribbon-wrapper-green"><div class="ribbon-green">Popular</div></div>
             </div>
             <?php
-            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/books_img_03.png"), $this->createUrl('/web/product/allproducts'));
+            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/books_img_03.png"), $this->createUrl('/web/product/allproducts', array('country' => Yii::app()->session['country_short_name'], 'city' => Yii::app()->session['city_short_name'], 'city_id' => Yii::app()->session['city_id'])));
             ?>
             <h4>Books</h4>
             <article>Lorem ipsum color sit bla bla thhm ipoum deona eio a ea sho moxnt</article>
@@ -23,7 +23,7 @@
         </div>
         <div class="landing_div">
             <?php
-            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/educational_toys_img_03.png"), $this->createUrl('/web/product/allproducts'));
+            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/educational_toys_img_03.png"), $this->createUrl('/web/educationToys/index', array('country' => Yii::app()->session['country_short_name'], 'city' => Yii::app()->session['city_short_name'], 'city_id' => Yii::app()->session['city_id'])));
             ?>
             <h4>Educationsl Toys</h4>
             <article>Lorem ipsum color sit bla bla thhm ipoum deona eio a ea sho moxnt</article>
@@ -31,7 +31,7 @@
         </div>
         <div class="landing_div">
             <?php
-            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/other_items_img_03.png"), $this->createUrl('/web/product/allproducts'));
+            echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/other_items_img_03.png"), $this->createUrl('/web/others/index', array('country' => Yii::app()->session['country_short_name'], 'city' => Yii::app()->session['city_short_name'], 'city_id' => Yii::app()->session['city_id'])));
             ?>
             <h4>Other Items</h4>
             <article>Lorem ipsum color sit bla bla thhm ipoum deona eio a ea sho moxnt</article>
