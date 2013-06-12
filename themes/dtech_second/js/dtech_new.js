01b6d3bef33b3c245f7bc6793126f7408867167f
aliabbas <aliabbas@localhost>
2013-06-12 16:18:51 +0500

    New theme

---
+++ themes/dtech_second/js/dtech_new.js
@@ -0,0 +1,129 @@
+var dtech_new  = {
+	popupStatus : 0,
+	toggleLogin : function (){
+		var button = jQuery('#login_btn');
+		var box = jQuery('#login_bx');
+		var form = jQuery('#login_frm');
+		button.removeAttr('href');
+		button.mouseup(function(ev) {
+			box.toggle();
+			button.toggleClass('active');
+		});
+		form.mouseup(function() { 
+			return false;
+		});
+		jQuery(this).mouseup(function(login) {
+			if(!(jQuery(login.target).parent('#login_btn').length > 0)) {
+				button.removeClass('active');
+				box.hide();
+			}
+		});
+	},
+	toggleSideBar : function (){
+		var button = jQuery('#sideBarButton');
+		var box = jQuery('#sideBarBox');
+		var form = jQuery('#sideBarForm');
+		button.removeAttr('href');
+		button.mouseup(function(ev) {
+		
+			box.toggle('slow');
+			button.toggleClass('active');
+		});
+		form.mouseup(function() { 
+			return false;
+		});
+		jQuery("#sideBarBox").click(function(){
+				jQuery("#sideBarBox").show();
+				//console.log("helo");
+		})
+	
+		jQuery(this).mouseup(function(login) {
+			if(!(jQuery(login.target).parent('#sideBarButton').length > 0)) {
+				button.removeClass('active');
+				box.hide();
+			}
+		});
+	
+		
+	},
+	footerToggle : function(){
+		  jQuery('.btnToggle').click(function(){
+          jQuery('#dvText').slideToggle(300);			
+          return false;
+       });
+	},
+	
+	/*********** Listing page detail PopUp *****************************/
+	registerPopUp : function (){
+		$("a.topopup").click(function() {
+			dtech_new.loading(); // loading
+			setTimeout(function(){ // then show popup, deley in .5 second
+				dtech_new.loadPopup(); // function show popup 
+			}, 500); // .5 second
+			return false;
+		}); // end of event
+		
+		
+		$("div.close").hover(
+					function() {
+						$('span.ecs_tooltip').show();
+					},
+					function () {
+    					$('span.ecs_tooltip').hide();
+  					}
+				);
+	
+		$("div.close").click(function() {
+			dtech_new.disablePopup();  // function close pop up
+		});
+		
+		$(this).keyup(function(event) {
+			if (event.which == 27) { // 27 is 'Ecs' in the keyboard
+				dtech_new.disablePopup();  // function close pop up
+			}  	
+		});
+		
+		$("div#backgroundPopup").click(function() {
+			dtech_new.disablePopup();  // function close pop up
+		});
+		
+		$('a.livebox').click(function() {
+			alert('Hello World!');
+			return false;
+		});
+	},
+	changeBookImgHover : function (){	
+		$(".books_content a img").hover(
+			function () {
+				$(this).attr("src",$(this).attr("hover_img"));
+			},
+			function () {
+				$(this).attr("src",$(this).attr("unhover_img"));
+			}
+		);
+
+	},
+	loadPopup : function () { 
+		if(dtech_new.popupStatus == 0) { // if value is 0, show popup
+			dtech_new.closeloading(); // fadeout loading
+			$("#toPopup").fadeIn(0500); // fadein popup div
+			$("#backgroundPopup").css("opacity", "0.7"); // css opacity, supports IE7, IE8
+			$("#backgroundPopup").fadeIn(0001); 
+			dtech_new.popupStatus = 1; // and set value to 1
+		}	
+	},		
+	disablePopup : function() {
+		if(dtech_new.popupStatus == 1) { // if value is 1, close popup
+			$("#toPopup").fadeOut("normal");  
+			$("#backgroundPopup").fadeOut("normal");  
+			dtech_new.popupStatus = 0;  // and set value to 0
+		}
+	},
+	loading : function() {
+		$("div.loader").show();  
+	},
+	closeloading: function () {
+		$("div.loader").fadeOut('normal');  
+	}
+	
+}
---
+++ themes/dtech_second/views/layouts/_footer.php
@@ -0,0 +1,44 @@
+<div id="footer_part">
+    <div id="header_img">
+        <div class="toggleBtnHolder">
+            <img src="images/footer_open_img_03.png" class="btnToggle">
+            <div id="dvText">
+                <div id="div_text">
+                    <div id="left_footer">
+                        <h1>Connect to DARUSSALAM</h1>
+                        <a href="#"><img src="images/f_img_06.png" /></a>
+                        <a href="#"><img src="images/t_img_06.png" /></a>
+                        <a href="#"><img src="images/subs_img_06.png" /></a>
+                        <a href="#"><img src="images/you_img_06.png" /></a>
+                        <a href="#"><img src="images/subs_yellow_img_06.png" /></a>
+                        <a href="#"><img src="images/in_img_06.png" /></a>
+                        <div id="left_under_footer">
+                            <li><img src="images/phone_img_06.png" /> +(92) 42 35254654 - 54</li>
+                            <li><img src="images/mail_img_06.png" /> <a href="#">support@darussalam.com</a></li>
+                            <li><img src="images/home_img_06.png" /> Darussalam Publishers</li>
+                        </div>
+                        <p>is a multilingual international Islamic publishing house, with headquarters in Riyadh, Kingdom of Saudi Arabia.</p>
+                    </div>
+                    <div id="middle_footer">
+                        <h1>Navigation</h1>
+                        <article><a href="#">About Us</a></article>
+                        <article><a href="#">Contact Us</a></article>
+                        <article><a href="#">Careers</a></article>
+                        <article><a href="#">FAQ's</a></article>
+                        <article><a href="#">Terms &amp; Conditions</a></article>
+                        <article><a href="#">Shipping Rates & Policies</a></article>
+                    </div>
+                    <div id="right_footer">
+                        <h1>What's New?</h1>
+                        <p><a href="#">D-Tech - Working on technologies</a></p>
+                        <article><i>iPhone, Android & iPad Islamic apps</i></article>
+                        <p><a href="#">D-Tech - Working on technologies</a></p>
+                        <article><i>iPhone, Android & iPad Islamic apps</i></article>
+                        <section>&copy; 2013 Darussalam, Inc. All Rights Reserved.</section>
+                    </div>
+                </div>
+                <img src="images/down_footer_img_03.png" class="btnToggle" id="div_img" />
+            </div>
+        </div>
+    </div>
+</div>
