d39c442f19e497ba590034cdd132fa5c8179d332
ubaid <ubaidullah@darussalampk.com>
2013-06-17 12:03:46 +0500

    New code

--- themes/dtech_second/views/site/login.php
+++ themes/dtech_second/views/site/login.php
@@ -1,7 +1,7 @@
 <div id="login_content">
-    <?php 
-       echo CHtml::image(Yii::app()->theme->baseUrl."/images/shopping_cart_img_03.png"); 
-        ?>
+    <?php
+    echo CHtml::image(Yii::app()->theme->baseUrl . "/images/shopping_cart_img_03.png");
+    ?>
     <h6>Already a member?</h6>
     <div class="login_part">
         <p>User Name</p>
@@ -12,5 +12,15 @@
         <div id="main_login_pointer">
         </div>
         <input type="button" value="User Login" class="user_login_button" />
+    </div>
+    <div class="login_with_images">
+        <h4>Login with</h4>
+
+        <?php
+        echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/facebook_login_03.png"), $this->createUrl('/web/hybrid/login/', array("provider" => "facebook")));
+        echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/bird_login_03.png"), $this->createUrl('/web/hybrid/login/', array("provider" => "twitter")));
+        echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/google_login_03.png"), $this->createUrl('/web/hybrid/login/', array("provider" => "google")));
+        echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl . "/images/in_img_06.png"), $this->createUrl('/web/hybrid/login/', array("provider" => "linkden")));
+        ?>
     </div>
 </div>
