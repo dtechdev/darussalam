ad73cad149af78f55c23beda3ea56e133533f540
aliabbas <aliabbas@localhost>
2013-06-18 17:19:40 +0500

    New theme

---
+++ themes/dtech_second/views/layouts/_login _box.php
@@ -0,0 +1,38 @@
+<a href="#" id="login_btn">
+    <span>
+        Login 
+    </span>
+</a>
+<div style="clear:both"></div>
+<div id="login_bx">  
+    <?php
+    $login_model = new LoginForm;
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'login_frm',
+        'action' => Yii::app()->createUrl('/site/login'),
+        'enableClientValidation' => true,
+        'clientOptions' => array(
+            'validateOnSubmit' => true,
+        ),
+    ));
+    ?> 
+    <fieldset id="body">
+        <fieldset>
+            <div id="login_pointer">
+            </div>
+            <label for="email">User Name</label>
+            <?php
+            echo $form->textField($login_model, 'username', array("id" => "email"));
+            echo $form->hiddenField($login_model, 'route', array("value" => Yii::app()->request->getUrl()));
+            ?>
+        </fieldset>
+        <fieldset>
+            <label for="password">Password</label>
+            <?php
+            echo $form->passwordField($login_model, 'password', array("id" => "password"));
+            ?>
+        </fieldset>
+    </fieldset>
+    <?php echo CHtml::submitButton("User Login", array("class" => "user_login_btn")); ?>
+    <?php $this->endWidget(); ?>
+</div>
--- themes/dtech_second/views/layouts/main.php
+++ themes/dtech_second/views/layouts/main.php
@@ -44,7 +44,14 @@
                     </select>
                 </div>
                 <div id="right_header">
-                    <span><a href="#">Sign Up</a></span>
+                    <?php
+                    if (Yii::app()->user->isGuest){
+                        echo CHtml::openTag("span");
+                        echo '<a href="#">Sign Up</a>';
+                        echo CHtml::closeTag("span");
+                    }
+                    ?>
+                    
                     <span>
                         <div id="login_contain">
                             <?php
@@ -52,47 +59,8 @@
                                 echo $this->renderPartial("//layouts/_logout_box")
                                 ?>
                             <?php } else {
-                                ?>
-
-                                <a href="#" id="login_btn">
-                                    <span>
-                                        Login 
-                                    </span>
-                                </a>
-                                <div style="clear:both"></div>
-                                <div id="login_bx">  
-                                    <?php
-                                    $login_model = new LoginForm;
-                                    $form = $this->beginWidget('CActiveForm', array(
-                                        'id' => 'login_frm',
-                                        'action' => Yii::app()->createUrl('/site/login'),
-                                        'enableClientValidation' => true,
-                                        'clientOptions' => array(
-                                            'validateOnSubmit' => true,
-                                        ),
-                                    ));
-                                    ?> 
-                                    <fieldset id="body">
-                                        <fieldset>
-                                            <div id="login_pointer">
-                                            </div>
-                                            <label for="email">User Name</label>
-                                            <?php
-                                            echo $form->textField($login_model, 'username', array("id" => "email"));
-                                            echo $form->hiddenField($login_model, 'route', array("value" => Yii::app()->request->getUrl()));
-                                            ?>
-                                        </fieldset>
-                                        <fieldset>
-                                            <label for="password">Password</label>
-                                            <?php
-                                            echo $form->passwordField($login_model, 'password', array("id" => "password"));
-                                            ?>
-                                        </fieldset>
-                                    </fieldset>
-                                    <?php echo CHtml::submitButton("User Login", array("class" => "user_login_btn")); ?>
-                                    <?php $this->endWidget(); ?>
-                                </div>
-                            <?php } ?>
+                                $this->renderPartial("//layouts/_login_box");
+                            } ?>
                         </div>
                     </span>
                     <span>
