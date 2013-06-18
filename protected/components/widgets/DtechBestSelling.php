12824d50f8ff960d44c19c75ef76a190d8aa6350
ubaid <ubaidullah@darussalampk.com>
2013-06-18 11:09:23 +0500

    New code

---
+++ protected/components/widgets/DtechBestSelling.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * DtechSeonondSidebar
+ * it will be used to handle sidebar
+ * of current dtech second theme
+
+ * @author :Ali Abbas
+ * @version : since 1.1
+ * 
+
+ */
+Yii::import('zii.widgets.CPortlet');
+
+class DtechBestSelling extends CPortlet {
+
+    /**
+     *
+     * @var type 
+     */
+    public $cssFile, $route, $querystring, $jsFile;
+    
+    /**
+     * current controller object
+     * @var type 
+     */
+    public $cObj;
+    
+    /**
+     * isfilter through js
+     * @var type 
+     */
+    public $is_cat_filter = 0;
+
+    /**
+     * by default it ajax based
+     * @var type 
+     */
+    public $ajax = true;
+
+    /**
+     * Init 
+     */
+    public function init() {
+        parent::init();
+       
+
+        /** in case of querystring ** for document linking */
+        if (Yii::app()->request->getQueryString() != "") {
+            $this->querystring = Yii::app()->request->getQueryString();
+            if ($this->querystring != "") {
+                $this->querystring = "&" . $this->querystring;
+            }
+        }
+    }
+
+    /**
+     * 
+     */
+    public function run() {
+        parent::run();
+
+
+        $cs = Yii::app()->clientScript;
+        if (!empty($this->jsFile)) {
+            $cs->registerScriptFile($this->jsFile);
+        }
+        if (!empty($this->cssFile)) {
+            $cs->registerCssFile($this->cssFile);
+        }
+    }
+
+    /**
+     * 
+     */
+    protected function renderContent() {
+        
+        $this->render('dtechBestSelling');
+    }
+
+}
+
+?>
---
+++ protected/components/widgets/views/dtechBestSelling.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<a href="javascript:void(0)" onClick="dtech_new.showBestSeller()">
+
+    <?php
+    echo CHtml::image(Yii::app()->theme->baseUrl . "/images/best_sellers_img_03.png");
+    ?>
+</a>
+<div class="under_best_seller">
+
+    <?php
+    echo CHtml::image(Yii::app()->theme->baseUrl . "/images/crown_img_03.png", '', array("class" => "crown_img"));
+    ?>
+    <h1>Best Sellers</h1>
+
+    <?php
+    $order_detail = new OrderDetail;
+    $dataProvider = $order_detail->bestSellings(5);
+    $bestSellings = $order_detail->getBestSelling($dataProvider);
+
+//                                $this->dtdump($bestSellings);
+//                                die;
+
+    foreach ($bestSellings as $bests) {
+        /*
+          $rating_value = ProductReviews::model()->calculateRatingValue($bests['product_id']);
+          $this->widget('CStarRating', array(
+          'name' => 'ratings',
+          'minRating' => 1,
+          'maxRating' => 5,
+          'starCount' => 5,
+          'value' => round($rating_value),
+          'readOnly' => true,
+          ));die;
+         * 
+         */
+        $pro_name = $bests['product_name'];
+        $orders = $bests['totalOrder'];
+        $image = $bests['no_image'];
+
+        if (isset($bests['image'][0]['image_small'])) {
+            $image = $bests['image'][0]['image_small'];
+        }
+        echo CHtml::openTag("div", array("class" => "quran_pen"));
+        echo CHtml::openTag("div", array("class" => "quran_img"));
+        echo CHtml::link(CHtml::image($image, $pro_name, array('style' => 'width:90px;height:140px')), $this->cObj->createUrl('/web/product/productDetail', array('product_id' => $bests['product_id'])), array('title' => $pro_name));
+        echo CHtml::closeTag("div");
+        echo CHtml::openTag("div", array("class" => "quran_text"));
+        echo CHtml::openTag("h2");
+        echo CHtml::link(implode(' ', array_slice(explode(' ', $pro_name), 0, 4)), $this->cObj->createUrl('/web/product/productDetail'));
+        echo CHtml::closeTag("h2");
+        echo CHtml::openTag("p");
+        echo $bests[product_description];
+        echo CHtml::closeTag("p");
+        echo CHtml::openTag("article");
+        echo CHtml::image(Yii::app()->theme->baseUrl . "/images/good_stars_img_03.png");
+        echo CHtml::closeTag("article");
+        echo CHtml::closeTag("div");
+        echo CHtml::closeTag("div");
+
+        echo CHtml::openTag("div", array("class" => "shop_up"));
+
+        echo CHtml::ajaxButton('Shop Now', $this->cObj->createUrl('/cart/addtocart'), array('data' => array(
+                'product_profile_id' => $product->productProfile[0]->id,
+                'city_id' => !empty($_REQUEST['city_id']) ? $_REQUEST['city_id'] : Yii::app()->session['city_id'],
+                'city' => !empty($_REQUEST['city_id']) ? $_REQUEST['city_id'] : Yii::app()->session['city_id'],
+                'quantity' => 'js:jQuery(\'#quantity\').val()'
+            ),
+            'type' => 'POST',
+            'dataType' => 'json',
+            'success' => 'function(data){
+                                           //jQuery(".cart_quantity").trigger("change");
+                                           dtech.custom_alert("Item has added to cart" ,"Add to Cart");
+                                           
+                                      }',
+                ), array('class' => 'shop_now_arrow')
+        );
+
+
+        // echo CHtml::button('Shop Now', array('class' => 'shop_now_arrow'));
+        echo CHtml::closeTag("div");
+    }
+    ?>
+
+
+
+<!--    <div class="quran_pen">
+        <div class="quran_img">
+
+            <?php
+            //echo CHtml::image(Yii::app()->theme->baseUrl . "/images/quran_pen_img_03.png");
+            ?>
+        </div>
+        <div class="quran_text">
+            <h2>Quran Pen</h2>
+            <p>Lorem ipsum color sit bla bla thhm ipoum deona eio a ea sho moxnt</p>
+            <article>
+
+                <?php
+               // echo CHtml::image(Yii::app()->theme->baseUrl . "/images/good_stars_img_03.png");
+                ?>
+                (7)
+            </article>
+        </div>
+    </div>
+    <div class="shop_up">
+        <input class="shop_now_arrow" type="button" value="Shop Now >" />
+    </div>-->
+</div>
