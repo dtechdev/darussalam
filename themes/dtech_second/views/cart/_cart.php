bca70be995b9835f2f2a42b0ab253fd047f454d9
aliabbas <aliabbas@localhost>
2013-06-19 16:55:38 +0500

    User status

--- themes/dtech_second/views/cart/_cart.php
+++ themes/dtech_second/views/cart/_cart.php
@@ -3,39 +3,39 @@
     <div id="pointer">
     </div>
     <h1>My Shopping Bag</h1>
-    <?php
-    $grand_total = 0;
-    $total_quantity = 0;
-    foreach ($cart as $pro) {
-        $grand_total = $grand_total + ($pro->quantity * $pro->productProfile->price);
-        $total_quantity+=$pro->quantity;
-        
-      
-        ?>
-        <div class="sub-sub-menu">
-            <?php
-            
-            echo CHtml::textField('quantity' . $pro->cart_id, $pro->quantity,
-               array(
-                   "class"=>"_small_cart_text",
-                   "onkeyup"=>"
-                            dtech_new.updateCart('".$this->createUrl('/web/cart/editcart')."',this,'".$pro->cart_id."');
-                    ")     
-            );
-            ?>
-            <h2><?php echo substr($pro->productProfile->product->product_name, 0, 10) . ".."; ?></h2>
-            <span>
-                <span class="unit_price">
-                     <?php echo round($pro->productProfile->price, 2); ?> =
-                </span>
-               
-                <span class="sub_total"><?php echo round($pro->quantity * $pro->productProfile->price, 2); ?> </span>
-            </span>
-            
-        </div>
+    <div class="sub-menu-child">
+
+
         <?php
-    }
-    ?>
+        $grand_total = 0;
+        $total_quantity = 0;
+        foreach ($cart as $pro) {
+            $grand_total = $grand_total + ($pro->quantity * $pro->productProfile->price);
+            $total_quantity+=$pro->quantity;
+            ?>
+            <div class="sub-sub-menu">
+                <?php
+                echo CHtml::textField('quantity' . $pro->cart_id, $pro->quantity, array(
+                    "class" => "_small_cart_text",
+                    "onkeyup" => "
+                            dtech_new.updateCart('" . $this->createUrl('/web/cart/editcart') . "',this,'" . $pro->cart_id . "');
+                    ")
+                );
+                ?>
+                <h2><?php echo substr($pro->productProfile->product->product_name, 0, 10) . ".."; ?></h2>
+                <span>
+                    <span class="unit_price">
+                        <?php echo round($pro->productProfile->price, 2); ?> =
+                    </span>
+
+                    <span class="sub_total"><?php echo round($pro->quantity * $pro->productProfile->price, 2); ?> </span>
+                </span>
+
+            </div>
+            <?php
+        }
+        ?>
+    </div>
     <div class="total">
         <?php
         echo CHtml::image(Yii::app()->theme->baseUrl . "/images/total_little_img_03.png");
