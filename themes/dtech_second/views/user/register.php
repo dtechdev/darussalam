3c7f13ab473ed14ad785c85b4055468f8161b541
ubaid <ubaidullah@darussalampk.com>
2013-06-19 17:31:10 +0500

    new theme

--- themes/dtech_second/views/user/register.php
+++ themes/dtech_second/views/user/register.php
@@ -1,3 +1,19 @@
+<?php
+$this->webPcmWidget['filter'] = array('name' => 'DtechSecondSidebar',
+    'attributes' => array(
+        'cObj' => $this,
+        'cssFile' => Yii::app()->theme->baseUrl . "/css/side_bar.css",
+        'is_cat_filter' => 1,
+        ));
+?>
+<?php
+$this->webPcmWidget['best'] = array('name' => 'DtechBestSelling',
+    'attributes' => array(
+        'cObj' => $this,
+        'cssFile' => Yii::app()->theme->baseUrl . "/css/side_bar.css",
+        'is_cat_filter' => 0,
+        ));
+?>
 <div id="user_login">
     <div id="main_user_login">
         <?php echo $this->renderPartial('//user/_sign_up', array('model' => $model)); ?>
