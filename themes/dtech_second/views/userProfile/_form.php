5a276dafabf1a9ab9fabe6efd3361509e080c600
ubaid <ubaidullah@darussalampk.com>
2013-06-20 12:16:21 +0500

    new theme

--- themes/dtech_second/views/userProfile/_form.php
+++ themes/dtech_second/views/userProfile/_form.php
@@ -14,7 +14,7 @@ $form = $this->beginWidget(
 
         <?php
         if (Yii::app()->user->hasFlash('profie_success')) {
-            echo CHtml::openTag("span",array("class"=>"flash"));
+            echo CHtml::openTag("span", array("class" => "flash"));
             echo Yii::app()->user->getFlash('profie_success');
             echo CHtml::closeTag("span");
         }
@@ -25,197 +25,187 @@ $form = $this->beginWidget(
             <article><span>*</span>Mandatory Fields</article>
         </div>
 
-<?php
-echo CHtml::submitButton("Save", array("class" => "row_button"))
-?>
+        <?php
+        echo CHtml::submitButton("Save", array("class" => "row_button"))
+        ?>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'avatar'); ?>
+                    <?php echo $form->labelEx($model, 'avatar'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php
-echo CHtml::image($model->uploaded_img, '', array(
-    "width" => "80",
-    "height" => "80",
-    "style" => "cursor:pointer",
-    "onclick" => "$('#UserProfile_avatar').trigger('click')",
-));
-?>
+                <?php
+                echo CHtml::image($model->uploaded_img, '', array(
+                    "width" => "80",
+                    "height" => "80",
+                    "style" => "cursor:pointer",
+                    "onclick" => "$('#UserProfile_avatar').trigger('click')",
+                ));
+                ?>
                 <?php echo $form->fileField($model, 'avatar', array("style" => "display:none")); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'first_name'); ?>
+                    <?php echo $form->labelEx($model, 'first_name'); ?>
                 </article>
             </div>
             <div class="row_input_type">
 
-<?php echo $form->textField($model, 'first_name', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'first_name', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'last_name'); ?>
+                    <?php echo $form->labelEx($model, 'last_name'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'last_name', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'last_name', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-
+                    <?php echo $form->labelEx($model, 'gender'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'last_name', array('class' => 'row_text_type')); ?>
+                <?php
+                echo $form->dropDownList($model, 'gender', array('male' => 'Male', 'female' => 'Female'), $htmlOptions = array('class' => 'row_drop_down', 'options' => array('1' => array('selected' => true))));
+                ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'gender'); ?>
+                    <?php echo $form->labelEx($model, 'date_of_birth'); ?> 
                 </article>
             </div>
             <div class="row_input_type">
-<?php
-echo $form->dropDownList($model, 'gender', array('male' => 'Male', 'female' => 'Female'), $htmlOptions = array('class' => 'row_drop_down', 'options' => array('1' => array('selected' => true))));
-?>
+                <?php
+                $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                    'model' => $model,
+                    'attribute' => 'date_of_birth',
+                    'options' => array(
+                        'mode' => 'focus',
+                        'dateFormat' => Yii::app()->params['dateformat'],
+                        'buttonImage' => Yii::app()->baseUrl . '/images/date_picker.png',
+                        'buttomImageOnly' => true,
+                        'buttonText' => '',
+                        'showAnim' => 'fold',
+                        'showOn' => 'button',
+                        'showButtonPanel' => false,
+                        'showAnim' => 'slideDown',
+                        'changeYear' => true,
+                        'changeMonth' => true,
+                        'yearRange' => '1930',
+                    ),
+                    'htmlOptions' => array(
+                        'size' => '15', // textField size
+                        //'value' => date("d F, Y"),
+                        'maxlength' => '10', // textField maxlength
+                        'class' => 'row_text_type', // textField maxlength
+                        'readOnly' => TRUE,
+                        'style' => 'width:210px',
+                    ),
+                ));
+                ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'date_of_birth'); ?> 
+                    <?php echo $form->labelEx($model, 'address'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php
-$this->widget('zii.widgets.jui.CJuiDatePicker', array(
-    'model' => $model,
-    'attribute' => 'date_of_birth',
-    'options' => array(
-        'mode' => 'focus',
-        'dateFormat' => Yii::app()->params['dateformat'],
-        'buttonImage' => Yii::app()->baseUrl . '/images/date_picker.png',
-        'buttomImageOnly' => true,
-        'buttonText' => '',
-        'showAnim' => 'fold',
-        'showOn' => 'button',
-        'showButtonPanel' => false,
-        'showAnim' => 'slideDown',
-        'changeYear' => true,
-        'changeMonth' => true,
-        'yearRange' => '1930',
-    ),
-    'htmlOptions' => array(
-        'size' => '15', // textField size
-        //'value' => date("d F, Y"),
-        'maxlength' => '10', // textField maxlength
-        'class' => 'row_text_type', // textField maxlength
-        'readOnly' => TRUE,
-        'style' => 'width:210px',
-    ),
-));
-?>
+                <?php echo $form->textField($model, 'address', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'address'); ?>
+                    <?php echo $form->labelEx($model, 'address_2'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'address', array('class' => 'row_text_type')); ?>
-            </div>
-        </div>
-        <div class="row_input">
-            <div class="row_text">
-                <article>
-<?php echo $form->labelEx($model, 'address_2'); ?>
-                </article>
-            </div>
-            <div class="row_input_type">
-<?php echo $form->textField($model, 'address_2', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'address_2', array('class' => 'row_text_type')); ?>
             </div>
         </div>
 
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'country'); ?>
+                    <?php echo $form->labelEx($model, 'country'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php $lstData = CHtml::listData(Country::model()->findAll(), 'country_name', 'country_name') ?>
+                <?php $lstData = CHtml::listData(Country::model()->findAll(), 'country_name', 'country_name') ?>
                 <?php echo $form->dropDownList($model, 'country', $lstData, $htmlOptions = array('class' => 'row_drop_down')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'city'); ?>
+                    <?php echo $form->labelEx($model, 'city'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'city', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'city', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'state_province'); ?>
+                    <?php echo $form->labelEx($model, 'state_province'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'state_province', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'state_province', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'zip_code'); ?>
+                    <?php echo $form->labelEx($model, 'zip_code'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'zip_code', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'zip_code', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'contact_number'); ?>
+                    <?php echo $form->labelEx($model, 'contact_number'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'contact_number', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'contact_number', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'mobile_number'); ?>
+                    <?php echo $form->labelEx($model, 'mobile_number'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->textField($model, 'mobile_number', array('class' => 'row_text_type')); ?>
+                <?php echo $form->textField($model, 'mobile_number', array('class' => 'row_text_type')); ?>
             </div>
         </div>
         <div class="row_input">
             <div class="row_text">
                 <article>
-<?php echo $form->labelEx($model, 'is_shipping_address'); ?>
+                    <?php echo $form->labelEx($model, 'is_shipping_address'); ?>
                 </article>
             </div>
             <div class="row_input_type">
-<?php echo $form->checkBox($model, 'is_shipping_address', array('class' => 'account_text')); ?>
+                <?php echo $form->checkBox($model, 'is_shipping_address', array('class' => 'account_text')); ?>
             </div>
         </div>
     </div>
