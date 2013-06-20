2de74543c471d655c2ee092b0b370fb02894fd58
aliabbas <aliabbas@localhost>
2013-06-20 11:34:42 +0500

    ui cahnges

--- protected/models/UserUpdate.php
+++ protected/models/UserUpdate.php
@@ -24,7 +24,6 @@ class UserUpdate extends User {
             array('user_email', 'required'),
             array('create_time,create_user_id,update_time,update_user_id', 'required'),
             array('role_id, status_id, city_id, site_id', 'numerical', 'integerOnly' => true),
-           
             array('is_active', 'length', 'max' => 8),
             array('user_email', 'email'),
             array('user_email', 'unique'),
@@ -35,6 +34,16 @@ class UserUpdate extends User {
             array('special_offer,agreement_status', 'safe'),
             array('user_id, role_id, status_id, city_id, activation_key, is_active, site_id', 'safe', 'on' => 'search'),
         );
+    }
+
+    public function beforeSave() {
+        if (empty($this->join_date)) {
+            $this->join_date = date("Y-m-d");
+        } else {
+            /** in case of form is filling this value * */
+            $this->join_date = DTFunctions::dateFormatForSave($this->join_date);
+        }
+        return true;
     }
 
 }
