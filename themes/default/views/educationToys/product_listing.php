aef7497f2d9ae8e15c322a34e796f15a7e3499d0
aliabbas <aliabbas@localhost>
2013-06-24 14:29:40 +0500

    layout name

--- themes/default/views/educationToys/product_listing.php
+++ themes/default/views/educationToys/product_listing.php
@@ -1,6 +1,6 @@
 <div id="book_content">
     <div id="book_main_content">
-        <?php //$this->renderPartial("/product/_subheader"); ?>
+        <?php $this->renderPartial("//product/_subheader"); ?>
 
         <?php
         if (Yii::app()->controller->action->id != "getSearch") {
@@ -12,12 +12,12 @@
                 //echo (isset($_POST['serach_field'])?$_POST['serach_field']:"");
             echo CHtml::closeTag("div");
             
-            $this->renderPartial("/educationToys/_product_side_bar", array("allCate" => $allCate));
+            $this->renderPartial("//educationToys/_product_side_bar", array("allCate" => $allCate));
             
         }
         ?>
         <div id="right_main_content">
-            <?php $this->renderPartial("/educationToys/_product_list", 
+            <?php $this->renderPartial("//educationToys/_product_list", 
                     array(
                         "products" => $products,
                         'dataProvider'=>$dataProvider,
